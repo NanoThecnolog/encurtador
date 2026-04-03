0613d957a32a3bcc16552aa09673ec5ad3b2ea90
NanoThecnolog <contato@ericssongomes.com>
2026-04-03 20:08:39 -0300

    Adiciona versionamento de API

--- src/main.ts
+++ src/main.ts
@@ -2,31 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { DataAPIClient } from "@datastax/astra-db-ts";
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new HttpExceptionFilter())
 
-
-
-
-  /*const endpoint = process.env.ASTRA_DB_API_ENDPOINT
-  const dbToken = process.env.ASTRA_DB_APP_TOKEN
-  if (!endpoint || !dbToken) throw new Error("variáveis de ambiente do DB não definido")
-
-  const client = new DataAPIClient();
-
-  const db = client.db(endpoint, {
-    token: dbToken
-  });
-
-  (async () => {
-    const colls = await db.listCollections();
-    console.log('Connected to AstraDB:', colls);
-  })();*/
-
-
+  //versionamento da api
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  })
 
   const port = process.env.PORT
   if (!port) throw new Error("Porta não definida")
