a6cf2b1d0bee2b32b99705f3bfd126924aaa3d98
NanoThecnolog <contato@ericssongomes.com>
2026-04-04 17:45:21 -0300

    Ajustes no modulo geral

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { UrlModule } from './modules/url/url.module';
 import { UrlRepository } from './common/repositories/url.repository';
 import { AstraModule } from './common/providers/astra.module';
+import { RedirectModule } from './modules/redirect/redirect.module';
+import { ManageModule } from './modules/manage/manage.module';
 
 @Module({
   imports: [
@@ -13,6 +15,8 @@ import { AstraModule } from './common/providers/astra.module';
     }),
     UrlModule,
     AstraModule,
+    RedirectModule,
+    ManageModule,
   ],
   controllers: [AppController],
   providers: [AppService],
